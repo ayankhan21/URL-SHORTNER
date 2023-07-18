78c39e3b93fa96e64ce12c3330ea289785bd720f
ayankhan21 <ayankhanxyz00@gmail.com>
2023-07-18 19:32:26 +0530

    Done

--- urlshort/src/App.js
+++ urlshort/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-
-import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Navbarlink from './components/navbar';
 
@@ -9,9 +7,7 @@ const App = () => {
   return (
     <>
       <Navbarlink />
-      <Routes>
-        <Route exact path='/' element={<Home />}></Route>
-      </Routes>
+      <Home />
     </>
   )
 }
